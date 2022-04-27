23d6e5fc27502d07b2806cf626db6dfa55d13a84
dprondyaev <romashka14108@mail.ru>
2022-04-27 15:52:40 +0300

    first commit

---
+++ sql/db.sql
@@ -0,0 +1,12 @@
+DROP schema IF EXISTS otus;
+create schema otus;
+
+create table distance_statistics
+(
+    date text,
+    count bigint not null,
+    min double precision,
+    max double precision,
+    avg double precision,
+    "RMSD(trip_distance)" double precision
+);
